7315640ee8a0447212464bb2755140d7bede9eb3
nicius2 <marco.marks06@gmail.com>
2025-03-24 15:35:56 -0300

    feat: adicionando o header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import Header from "./components/Header"
+import styles from "./app.module.css"
 
+function handleRestart() {
+  alert("Reiniciar o jogo")
+}
 
-export default function App(){
+export default function App() {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <main>
+        <Header current={3} max={5} onRestart={handleRestart}/>
+      </main>
     </div>
   )
 }
